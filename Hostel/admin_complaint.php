2f5a299c91803fc9e59dece4ba617ff302584913
Priya Tare <priyatare1354@gmail.com>
2025-04-14 21:06:33 +0530

    Hostel folder

---
+++ Hostel/admin_complaint.php
@@ -0,0 +1,144 @@
+
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://localhost/Hostel/bootstrap/css/bootstrap.min.css" type =" text/css" >
+	<link rel="stylesheet" href="http://localhost/Hostel/bootstrap/css/glyphicon.css" type =" text/css" >
+	<script type ="text/javascript" src="http://localhost/Hostel/bootstrap/js/jquery.js"></script>
+	<script type ="text/javascript" src="http://localhost/Hostel/bootstrap/js/bootstrap.min.js"></script>
+	<title>Admin Complaint</title>
+	<style type="text/css">
+		*{
+			margin: 0;
+			padding: 0;
+			
+			font-family: Open Sans;
+		}
+		#text:hover
+{
+color: red;
+}
+#text
+{
+color: white ;
+}
+
+#image{
+		background:url("http://localhost/Hostel/images/img3.jpg");
+background-size: cover;
+height:100vh;
+
+		}
+		.container{
+			font-size: 18px;
+		}
+
+	</style>
+</head>
+<body >
+	<?php
+$host="localhost";
+	$duser="root";
+	$pass="";
+	$dbname="hostel_management";
+	
+	$con= mysqli_connect($host,$duser,$pass,$dbname)or die ("Connection Failed!");
+	?>
+
+<div id ="image" class="image-responsive">
+	<nav class="navbar navbar-inverse navbar-fixed-top  navbar-dark bg-dark navbar-expand-md " > <!--navbar-default class cntains default navbar in bootstrap-->
+ <div class="container-fluid">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle d-sm-none" data-toggle="collapse" data-target="#myNavbar">
+    <span class="glyphicon glyphicon-menu-hamburger"></span>
+    </button>
+    <a  id="text" href="first_page.php" class="navbar-brand " ><span class="glyphicon glyphicon-home"></span>  Hostel Management system</a>
+  </div>
+  <div class="navbar-right">
+    <div class="collapse navbar-collapse " id="myNavbar">
+        <ul class="nav navbar-nav">
+        <li> <a href ="admin_menu.php" id="text" class="nav-link navbar-brand" ><span class="glyphicon glyphicon-circle-arrow-left "></span></a></li>
+        </ul>
+    </div>
+  </div>
+ </div>
+</nav>
+	<div class="container">
+		<br>
+<div class="row">
+	<form action="search1.php" method="POST">
+		<div class="form-group">
+			<button type="submit" name="Search" class="btn btn-primary btn-sm">Search</button>&nbsp
+			<input type="text" name="value" class="form-control-sm" placeholder="Enter Here" autocomplete="off" required>
+			<select class="form-select" name="select">
+				<option value="room-no">Room no</option>
+				<option value="id">Complaint Id</option>
+
+			</select>
+		</div>
+	</form>
+
+<div class="col-lg-12 text-center border rounded bg-light my-5" >
+<h1> Complaint Details</h1>
+</div>
+
+
+<div class="col-lg-12">
+
+<table border = "1px" cellspacing ="30px" class="table table-bordered  table-sm font-weight-bold " >
+  <thead class="text-center table-danger">
+    <tr>
+    	<th scope="col">Complaint ID</th>
+    <th scope="col">Date</th>
+      <th scope="col">Room No</th>
+      <th scope="col">Name</th>
+      <th scope="col">Type of complaint</th>
+      <th scope="col">Description</th>
+      <th scope="col">Send reply</th>
+    </tr>
+  </thead>
+  <tbody class="text-center " style="color: white;">
+  	<?php
+
+$query="SELECT * FROM `complaint` ORDER by id desc   ";
+  $result=mysqli_query($con,$query);
+  
+  while($student=mysqli_fetch_assoc($result))
+  { 
+
+echo"
+	
+	 
+	 <td>$student[Id]</td> 
+	 
+	  <td>$student[Date]</td>
+	  <td>$student[room]</td>
+	  <td>$student[name]</td>
+	  <td>$student[type]</td>
+	  <td>$student[description]</td>
+	  <td><a href ='reply.php?id=$student[Id]&date=$student[Date]&room=$student[room]&name=$student[name]&type=$student[type]&description=$student[description]'>
+	  <input type ='submit' name='reply' value='Reply' class = 'btn btn-primary'></a></td>
+	  	";
+
+	  	?>
+	  	
+	  	<?php
+	  echo"
+	  </td>
+	  </tr>
+	  ";
+	  
+  }
+  	?>
+  </tbody>
+</table>
+</div>
+</div>
+</div>
+
+
+</body>
+</html>
